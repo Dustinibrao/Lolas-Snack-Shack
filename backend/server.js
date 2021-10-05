6d3dbd05f5efd8336195e603c12738b705a6a7f1
Dustin <dustinibrao@gmail.com>
2021-10-04 15:02:05 -1000

    connected to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,27 @@
 import express from "express";
+import mongoose from "mongoose";
 import data from "./data.js";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
+mongoose
+	.connect(
+		"mongodb+srv://dustin:Gecko123@cluster0.8mmaf.mongodb.net/lolasSnackShack?retryWrites=true&w=majority",
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		}
+	)
+	.then((res) => console.log("db connected"))
+	.catch((err) => console.log(err));
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+	// we're connected!
+	console.log("Testing mongoose db.once method");
+});
 
 app.get("/api/products/:id", (req, res) => {
 	const product = data.products.find((x) => x._id === req.params.id);
@@ -16,8 +36,13 @@ app.get("/api/products", (req, res) => {
 	res.send(data.products);
 });
 
+app.use("/api/users", userRouter);
+
 app.get("/", (req, res) => {
 	res.send("server is ready");
+});
+app.use((err, req, res, next) => {
+	res.status(500).send({ message: err.message });// error catcher
 });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
