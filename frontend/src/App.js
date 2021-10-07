8100cbedfb01887d1869947e0a73f112acb488da
Dustin <dustinibrao@gmail.com>
2021-10-06 15:24:36 -1000

    user can now sign in / out

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Link, Route } from "react-router-dom";
+import { signout } from "./actions/userActions";
 import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import SigninScreen from "./screens/SigninScreen";
 
 function App() {
 	const cart = useSelector((state) => state.cart);
 	const { cartItems } = cart;
+	const userSignin = useSelector((state) => state.userSignin);
+	const { userInfo } = userSignin;
+	const dispatch = useDispatch();
+	const signoutHandler = () => {
+		dispatch(signout());
+	};
 
 	return (
 		<BrowserRouter>
@@ -27,7 +35,26 @@ function App() {
 								</span>
 							)}
 						</Link>
-						<Link to="/signIn">Sign In</Link>
+						{userInfo ? (
+							<div className="dropdown">
+								<Link to="#">
+									{userInfo.name}
+									<i className="fa fa-caret-down">{""}</i>
+								</Link>
+								<ul className="dropdown-content">
+									<li>
+										<Link
+											to="#signout"
+											onClick={signoutHandler}
+										>
+											Sign Out
+										</Link>
+									</li>
+								</ul>
+							</div>
+						) : (
+							<Link to="/signIn">Sign In</Link>
+						)}
 					</div>
 				</header>
 				<main>
@@ -36,6 +63,7 @@ function App() {
 						path="/product/:id"
 						component={ProductScreen}
 					></Route>
+					<Route path="/signin" component={SigninScreen}></Route>
 					<Route path="/" component={HomeScreen} exact></Route>
 				</main>
 				<footer className="row center">All rights reserved</footer>
