eafeef7513ebaa4677e7b01dba6816290a46a6d0
Dustin <dustinibrao@gmail.com>
2021-10-08 11:27:46 -1000

    made payment screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Link, Route } from "react-router-dom";
 import { signout } from "./actions/userActions";
 import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
+import PaymentMethodScreen from "./screens/PaymentMethodScreen";
 import ProductScreen from "./screens/ProductScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ShippingAdressScreen from "./screens/ShippingAdressScreen";
@@ -70,6 +71,10 @@ function App() {
 					<Route
 						path="/shipping"
 						component={ShippingAdressScreen}
+					></Route>
+					<Route
+						path="/payment"
+						component={PaymentMethodScreen}
 					></Route>
 					<Route path="/" component={HomeScreen} exact></Route>
 				</main>
