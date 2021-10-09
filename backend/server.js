4c4b918d5f881c16502fde4b7530bfcfe68ac09e
Dustin <dustinibrao@gmail.com>
2021-10-09 11:12:06 -1000

    can now get directed after placing order

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
+import orderRouter from "./routers/orderRouter.js";
 dotenv.config();
 
 const app = express();
@@ -31,6 +32,7 @@ db.once("open", function () {
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
 app.get("/", (req, res) => {
 	res.send("server is ready");
 });
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,5 +1,6 @@
 import {
 	CART_ADD_ITEM,
+	CART_EMPTY,
 	CART_REMOVE_ITEM,
 	CART_SAVE_PAYMENT_METHOD,
 	CART_SAVE_SHIPPING_ADDRESS,
@@ -34,6 +35,8 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
 			return { ...state, paymentMethod: action.payload };
 		case CART_SAVE_SHIPPING_ADDRESS:
 			return { ...state, shippingAddress: action.payload };
+		case CART_EMPTY:
+			return { ...state, cartItems: [] };
 		default:
 			return state;
 	}
